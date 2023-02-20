0df50e4920aebbe8d244eded2ca446f77a13759a
Emilia Fredriksson <frem22pu@student.ju.se>
2023-02-20 14:37:33 +0100

    startup game

---
+++ lunarLander.js
@@ -0,0 +1,20 @@
+function background() {
+  background(0);
+}
+
+function planets(x, y, s) {
+  fill(255);
+  ellipse(x * s, y - 100 * s, 100 * s);
+  ellipse(x - 150 * s, y * s, 80 * s);
+  ellipse(x + 150 * s, y - 150 * s, 80 * s);
+  ellipse(x - 250 * s, y - 75 * s, 60 * s);
+  ellipse(x + 300 * s, y - 50 * s, 60 * s);
+  ellipse(x - 300 * s, y + 50 * s, 45 * s);
+  ellipse(x + 200 * s, y + 25 * s, 45 * s);
+  ellipse(x - 50 * s, y + 50 * s, 35 * s);
+  ellipse(x + 300 * s, y + 25 * s, 35 * s);
+}
+
+function draw() {
+  planets(350, 450, 1);
+}
