29312e1b7e26387e9b0321ff271b430aa31b4758
Emilia Fredriksson <frem22pu@student.ju.se>
2023-02-24 10:19:03 +0100

    planet color

--- lunarLander.js
+++ lunarLander.js
@@ -3,19 +3,41 @@ function scenery() {
 }
 
 function planets(x, y, s) {
-  fill(255);
+  noStroke();
+  fill(255, 128, 0);
   ellipse(x * s, y - 100 * s, 100 * s);
+
+  fill(102, 204, 0);
   ellipse(x - 150 * s, y * s, 80 * s);
+
+  fill(204, 204, 0);
   ellipse(x + 125 * s, y - 150 * s, 80 * s);
+
+  fill(255, 255, 153);
   ellipse(x - 250 * s, y - 75 * s, 60 * s);
+
+  fill(0, 102, 204);
   ellipse(x + 275 * s, y - 50 * s, 60 * s);
+
+  fill(153, 76, 0);
   ellipse(x - 300 * s, y + 50 * s, 45 * s);
+
+  fill(0, 204, 204);
   ellipse(x + 200 * s, y + 25 * s, 45 * s);
+
+  fill(255, 51, 51);
   ellipse(x - 50 * s, y + 50 * s, 35 * s);
+
+  fill(255, 204, 53);
   ellipse(x + 350 * s, y + 25 * s, 35 * s);
+}
+
+function rocket(x, y, s) {
+  noStroke();
 }
 
 function draw() {
   scenery();
   planets(350, 450, 1);
+  rocket(400, 50, 4);
 }
